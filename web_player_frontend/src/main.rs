6ff6c8c17feba7adfeb2e545a3acb1c0165b465e
Evan Coulson <evan.m.coulson@gmail.com>
2024-04-30 04:01:06 +0000

    Added web player and static serving

--- web_player_frontend/src/main.rs
+++ web_player_frontend/src/main.rs
@@ -2,6 +2,7 @@ use tokio::net::TcpListener;
 
 use askama_axum::Template;
 use axum::{response::IntoResponse, Router};
+use tower_http::services::ServeDir;
 
 pub mod hub {
     tonic::include_proto!("hub");
@@ -11,6 +12,7 @@ pub mod hub {
 #[template(
     source = r#"
     <h1>player</h1>
+    <script src="assets/web-player.js"></script>
     "#,
     ext = "html"
 )]
@@ -18,7 +20,9 @@ struct PlayerComponent {}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/get_player", axum::routing::get(get_player));
+    let app = Router::new()
+        .route("/get_player", axum::routing::get(get_player))
+        .nest_service("/assets", ServeDir::new("assets"));
     let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
